418678c70acc1f00c10dacb56dce1357cfacb054
Leonardo Jaques <leonardo.jaquesdev@outlook.com>
2024-06-03 23:15:34 -0300

    feat: logout and adm header condition

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,42 @@
-import { usuarios } from "@/lib/data";
+'use client'
+import { useUser } from "@/context/user-context";
+import LogOutButton from "@/icons/logout";
+import logout from "@/lib/actions/logout";
 import Link from "next/link";
 
 export default function Header() {
+    const { user, setUser } = useUser();
+    
+    async function handleLogout() {
+        await logout();
+        setUser(null);
+    }
+
     return (
-        <nav className="bg-gray-800 p-4">
-            <ul className="flex  gap-6">
-                <li><Link href={'/'} className="text-white hover:text-gray-300">Home</Link></li>
-                    <li><Link href={'/usuarios'} className="text-white hover:text-gray-300">Usuários</Link></li>
-                    <li><Link href={'/coordenador/'} className="text-white hover:text-gray-300">Coordenadores</Link></li>
-                <li><Link href={'/voto'} className="text-white hover:text-gray-300">Votos</Link></li>
+        <nav className="bg-gray-800 p-4 flex justify-between items-center text-white">
+            <ul className="flex gap-6">
+                <li>
+                    <Link href="/" className="text-white hover:text-gray-300">Home</Link>
+                </li>
+                {user?.is_adm && (
+                    <>
+                        <li>
+                            <Link href="/usuarios" className="text-white hover:text-gray-300">Usuários</Link>
+                        </li>
+                        <li>
+                            <Link href="/coordenador" className="text-white hover:text-gray-300">Coordenadores</Link>
+                        </li>
+                    </>
+                )}
+                <li>
+                    <Link href="/voto" className="text-white hover:text-gray-300">Votos</Link>
+                </li>
             </ul>
+
+            <button onClick={handleLogout} className="text-white hover:text-gray-300">
+                <LogOutButton />
+                Sair
+            </button>
         </nav>
-    )
+    );
 }
